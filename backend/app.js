8ae5830ad0e04b7f01c13e8d46f1b17950d81095
Vivek24411 <vvp44814@gmail.com>
2025-09-26 06:55:32 +0530

    logic

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use(cors(
       return callback(new Error(msg), false);
     }
     return callback(null, true);
+    
   },
   credentials: true // Enable credentials (cookies, authorization headers, etc.)
 }
